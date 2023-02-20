e8b66b80cc06e0d866c97d5730580e83998618b7
ruthzil <caio@citrangulo.com.br>
2023-02-20 17:48:34 -0300

    Update

---
+++ novaweb/src/test/java/br/com/ruthzil/novaweb/NovawebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ruthzil.novaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovawebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
